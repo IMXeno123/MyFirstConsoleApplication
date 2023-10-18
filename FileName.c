18e42ee789d9eb5dc065b7f562cf3703cff4da0a
xs <xs@asheng.onmicrosoft.com>
2023-10-18 10:53:54 -0700

    Add project files.

---
+++ FileName.c
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <wchar.h>
+#include <windows.system.h>
+
+
+int main() {
+	system("chcp 65001");
+	setlocale(LC_ALL, "chs"); 
+	//??
+
+	return 0;
+};
+
+int test() {
+	int input = 0;
+	int res_ = scanf("%d", &input);
+	if (input == 1)
+	{
+		wprintf(L"真");
+		int i;
+		for (i = 0; i < 5; i++) {
+			printf("\nfuck");
+		}
+	}
+	else {
+		wprintf(L"假");
+		printf("%d\n", res_);
+		int i = 1;
+		while (i <= 300) {
+			printf("%d shit\n", i);
+			i++;
+		}
+	}
+	return 1;
+} 
+
+int add(int x, int y) {
+	return x + y;
+}
+
+int array_() {
+	int arr[] = {0,1,2,3,4,5,6,7,8,9};
+	int i;
+	for (i = 0; i < 10; i++) {
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
